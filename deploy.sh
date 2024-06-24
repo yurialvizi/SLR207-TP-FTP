789f0470f8bdc10acd297ee436571aa1222247d8
yurialvizi <yurialvizi@gmail.com>
2024-06-24 14:58:47 +0200

    Update deploy script to deploy master

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,50 @@
 #!/bin/bash
- 
+
+# number of nodes
+n=3
+
 # your list of hosts file that should contain each host in a separate line
-computers=`cat machines.txt | xargs -I {} echo {}`
+nodes=$(head -n $n myftpclient/src/main/resources/machines.txt | xargs -I {} echo {})
+master=$(sed -n "$((n + 1))p" myftpclient/src/main/resources/machines.txt | xargs -I {} echo {})
+
 # your login 
 login="ydesene-23"
+
 # skip host key checking MAJOR SECURITY RISK :-DD
 sshopts="-o StrictHostKeyChecking=no"
- 
+
 remoteFolder="/dev/shm/$login/"
 mvn clean compile assembly:single
-fileName="myftpserver-1-jar-with-dependencies"
+
+nodeFileName="node-1-jar-with-dependencies"
 fileExtension=".jar"
- 
- 
-for c in ${computers[@]}; do
-  command0="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$c"
-  command1="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$c 'rm -rf $remoteFolder;mkdir $remoteFolder'"
-  command2="gpg -d -q ~/.password.gpg | sshpass scp $sshopts myftpserver/target/$fileName$fileExtension $login@$c:$remoteFolder$fileName$fileExtension"
-  echo ${command0[*]}
+
+for node in ${nodes[@]}; do
+  command0="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$node 'echo connected'"
+  command1="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$node 'rm -rf $remoteFolder; mkdir -p $remoteFolder'"
+  command2="gpg -d -q ~/.password.gpg | sshpass scp $sshopts myftpserver/target/$nodeFileName$fileExtension $login@$node:$remoteFolder$nodeFileName$fileExtension"
+  
+  echo "Connecting to $node..."
   eval $command0
-  echo ${command1[*]}
+  
+  echo "Preparing remote folder on $node..."
   eval $command1
-  echo ${command2[*]}
+  
+  echo "Copying file to $node..."
   eval $command2
 done
- 
-# for c in ${computers[@]}; do 
-#   while true; do
-#     command3="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$c 'cd $remoteFolder; nohup java -jar $fileName$fileExtension -Xmx4096m 1>/dev/null 2>/dev/null &'"
-#     echo $c
-#     echo ${command3[*]}
-#     $(eval $command3)
-#     if [ $? -ne 0 ]; then
-#       echo "Error while executing command, retrying"
-#     else
-#       break
-#     fi
-#   done
-# done
+
+masterFileName="master-1-jar-with-dependencies"
+
+command0="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$master 'echo connected'"
+command1="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$master 'rm -rf $remoteFolder; mkdir -p $remoteFolder'"
+command2="gpg -d -q ~/.password.gpg | sshpass scp $sshopts myftpclient/target/$masterFileName$fileExtension $login@$master:$remoteFolder$masterFileName$fileExtension"
+
+echo "Connecting to master node $master..."
+eval $command0
+
+echo "Preparing remote folder on master node $master..."
+eval $command1
+
+echo "Copying file to master node $master..."
+eval $command2
