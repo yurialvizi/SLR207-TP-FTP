90542a17c0cc77532bfd2e87946f2f3d9f51fa5c
yurialvizi <yurialvizi@gmail.com>
2024-06-28 02:42:56 +0200

    Refactor deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # number of nodes
-n=3
+n=18
 
 # your list of hosts file that should contain each host in a separate line
-nodes=$(head -n $n myftpclient/src/main/resources/machines.txt | xargs -I {} echo {})
-master=$(sed -n "$((n + 1))p" myftpclient/src/main/resources/machines.txt | xargs -I {} echo {})
+nodes=$(sed -n '2,$p' myftpclient/src/main/resources/machines.txt | head -n $n | xargs -I {} echo {})
+master=$(head -n 1 myftpclient/src/main/resources/machines.txt | xargs -I {} echo {})
 
 # your login 
 login="ydesene-23"
@@ -16,35 +16,53 @@ sshopts="-o StrictHostKeyChecking=no"
 remoteFolder="/dev/shm/$login/"
 mvn clean compile assembly:single
 
-nodeFileName="node-1-jar-with-dependencies"
-fileExtension=".jar"
+execute_remote_commands() {
+    local node=$1
+    local filePath=$2
+    local fileName=$3
+    local master_arg=$4
 
+    command0="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$node 'echo connected'"
+    command1="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$node 'rm -rf $remoteFolder; mkdir -p $remoteFolder'"
+    command2="gpg -d -q ~/.password.gpg | sshpass scp $sshopts $filePath$fileName $login@$node:$remoteFolder$fileName"
+    
+    if [ -n "$master_arg" ]; then
+        command3="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$node 'java -jar $remoteFolder$fileName $master_arg'"
+    else
+        command3="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$node 'nohup java -jar $remoteFolder$fileName -Xmx4096m > "${remoteFolder}output.log" 2>&1 &'"
+    fi
+
+    echo "Connecting to $node..."
+    eval $command0
+
+    echo "Preparing remote folder on $node..."
+    eval $command1
+
+    echo "Copying file to $node..."
+    eval $command2
+
+    echo "Executing jar file on $node..."
+    eval $command3
+}
+
+kill_process_on_port() {
+    local node=$1
+    local port=5524
+
+    kill_command="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$node 'fuser -k -n tcp $port'"
+    
+    echo "Killing process on port $port on $node..."
+    eval $kill_command
+}
+
+nodeFilePath="myftpserver/target/"
+nodeFileName="node-1-jar-with-dependencies.jar"
 for node in ${nodes[@]}; do
-  command0="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$node 'echo connected'"
-  command1="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$node 'rm -rf $remoteFolder; mkdir -p $remoteFolder'"
-  command2="gpg -d -q ~/.password.gpg | sshpass scp $sshopts myftpserver/target/$nodeFileName$fileExtension $login@$node:$remoteFolder$nodeFileName$fileExtension"
-  
-  echo "Connecting to $node..."
-  eval $command0
-  
-  echo "Preparing remote folder on $node..."
-  eval $command1
-  
-  echo "Copying file to $node..."
-  eval $command2
+    kill_process_on_port $node
+    execute_remote_commands $node $nodeFilePath $nodeFileName
 done
 
-masterFileName="master-1-jar-with-dependencies"
-
-command0="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$master 'echo connected'"
-command1="gpg -d -q ~/.password.gpg | sshpass ssh $sshopts $login@$master 'rm -rf $remoteFolder; mkdir -p $remoteFolder'"
-command2="gpg -d -q ~/.password.gpg | sshpass scp $sshopts myftpclient/target/$masterFileName$fileExtension $login@$master:$remoteFolder$masterFileName$fileExtension"
-
-echo "Connecting to master node $master..."
-eval $command0
-
-echo "Preparing remote folder on master node $master..."
-eval $command1
-
-echo "Copying file to master node $master..."
-eval $command2
+masterFilePath="myftpclient/target/"
+masterFileName="master-1-jar-with-dependencies.jar"
+echo "Executing master with argument $n"
+execute_remote_commands $master $masterFilePath $masterFileName $n
